02a4b430d0a4df59ae24f0854d9ab21a8522ab35
HeyItWorked <liamnguyenn98@gmail.com>
2021-09-22 18:27:21 -0500

    finish ex50

--- project_web/gothonweb/bin/webpy/app.py
+++ project_web/gothonweb/bin/webpy/app.py
@@ -1,20 +1,24 @@
 import web
-import sys
-sys.path.append(
-    '/Users/lamnguyen/Downloads/the hard way/project_web/gothonweb/bin/webpy-0.61')
-
 urls = (
-    '/', 'index'
+    '/', 'Index'
 )
 
 app = web.application(urls, globals())
 
+render = web.template.render('templates/')
 
-class index:
+
+class Index(object):
     def GET(self):
         greeting = "Hello World"
-        return greeting
+        # render.index because it's invoking index.html, this can be render. any html file
+        return render.index(greeting=greeting)
 
 
 if __name__ == "__main__":
     app.run()
+
+"""
+import web is possible by placing file near the folder your wish to import but that folder
+needs to have an empty __init__.py file
+"""
